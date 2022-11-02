32066364e36c1c04266ebaf52ec2d4d80c29b680
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2022-11-02 17:46:24 +0100

    Fix path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../lofarantpos/'))
+sys.path.insert(0, os.path.abspath('../..'))
 from lofarantpos import __version__
 
 project = 'lofarantpos'
