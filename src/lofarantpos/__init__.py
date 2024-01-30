177a035b047044a98d63138171731ad20b49477e
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2024-01-30 07:49:48 +0100

    Work around version thing for python 3.7

--- src/lofarantpos/__init__.py
+++ src/lofarantpos/__init__.py
@@ -1,3 +1,6 @@
-import importlib.metadata
+try:
+    import importlib.metadata
 
-__version__ = importlib.metadata.version("lofarantpos")
+    __version__ = importlib.metadata.version("lofarantpos")
+except ModuleNotFoundError:
+    __version__ = "Unknown (python 3.7 does not have importlib.metadata)"
